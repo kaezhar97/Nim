65b8db6dd3d8db92042cf925847a9d569d218f2d
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-10-20 17:06:01 -0400

    Pile class randomly generates a pile size

--- src/NimMainFiles/NimGameTest.java
+++ src/NimMainFiles/NimGameTest.java
@@ -5,6 +5,7 @@ public class NimGameTest
 {
     public static void main (String args[])
     {
-        
+        Pile gamePile=new Pile();
+        System.out.println("The starting pile has "+gamePile.getSize()+" marbles");
     }
 }
--- src/NimMainFiles/Pile.java
+++ src/NimMainFiles/Pile.java
@@ -1,15 +1,19 @@
 package NimMainFiles;
+import java.util.Random;
 
 
 public class Pile 
 {
+    private int pileSize;
+    
     public Pile()
     {
-        
+        Random pileGenerator = new Random();
+        pileSize=pileGenerator.nextInt(91)+10; // Generates a random pile size between 10 and 100 inclusive
     }
     
-    public int getNumberOfMarblesLeft()
+    public int getSize()
     {
-        return 0;
+        return pileSize;
     }
 }
